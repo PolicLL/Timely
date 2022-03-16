3010645fb9098c9ee46bad8c028c4ddee5f313f6
LPolic <48826059+PolicLL@users.noreply.github.com>
2022-03-16 09:59:35 +0100

    Update schema.sql

--- Timely/schema.sql
+++ Timely/schema.sql
@@ -1,4 +1,4 @@
- create table project (id integer not null, duration varchar(255), end_time timestamp, name varchar(255), start_time timestamp, primary key (id))
+create table project (id BIGINT NOT NULL DEFAULT nextval('project_seq') PRIMARY KEY, duration varchar(255), end_time timestamp, 
+name varchar(255), start_time timestamp);
 
- 
- 
+create sequence if not exists project_seq;
